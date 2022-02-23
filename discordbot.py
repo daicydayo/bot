39b8d6200a460bcdbf20fedc5d7f9b41f5d6d323
daicydayo <100273996+daicydayo@users.noreply.github.com>
2022-02-23 18:43:09 +0900

    check

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,27 @@
+import discord
 from discord.ext import commands
-from os import getenv
-import traceback
+#pip install requests
+import requests
 
-bot = commands.Bot(command_prefix='/')
-
+bot = commands.Bot(command_prefix = "m!")
 
 @bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
+async def on_ready():
+	print("準備完了")
+    
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def trans(ctx, *, msg):
+    trans_now = await ctx.send("日本語から英語に翻訳中です...")
+    api_key = "7238d01a-a7e1-750b-031c-26d3f5817920:fx"
+    params = {
+                "auth_key": api_key,
+                "text": str(msg),
+                "source_lang": "JA",
+                "target_lang": "EN"
+            }
 
+    request = requests.post("https://api-free.deepl.com/v2/translate", data=params)
+    result = request.json()
+    await trans_now.edit(content="JA → EN\n" + result["translations"][0]["text"])
 
-token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+bot.run(OTQ1OTU2ODU2NjEyOTI1NDcx.YhXs-Q.Ibt03FT3w4JvfENQrBxQIaxlFLY)
